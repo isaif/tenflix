cd47dedb7118f4b40810b7d07e067c6f2a9bdd43
isaif <saif935775@gmail.com>
2022-04-04 22:04:04 +0530

    feat: icons on accordion changes from '+' to 'x'

--- index.js
+++ index.js
@@ -3,26 +3,41 @@ const faqList = document.getElementById("faq-list");
 faqList.addEventListener("click", function (event) {
   const clickedElement = event.target;
 
+  // click may be on the button or any other child elements like the svg or path
+  // therefore we need to find the parent element which has "faq-question" class
   const faqQuestionElement = clickedElement.closest(".faq-question");
+  // for every question we have a sibling answer element which need to be shown
+  const targetFaqAnswer = faqQuestionElement.nextElementSibling;
 
-  let targetFaqAnswer = faqQuestionElement.nextElementSibling.classList;
+  const svgIcon = faqQuestionElement.querySelector(".svg-icon");
 
-  if (!targetFaqAnswer.contains("closed")) {
-    targetFaqAnswer.add("closed");
-  } else {
-    closeAll();
-    targetFaqAnswer.remove("closed");
-  }
+  addClassToCurrent(targetFaqAnswer, "faq-answer");
+  addClassToCurrent(svgIcon, "svg-icon");
 });
 
-function closeAll() {
-  const faqAnswers = document.getElementsByClassName("faq-answer");
+// function to add "closed" class to the targetElement
+function addClassToCurrent(targetElement, targetElementClassname) {
+  const classList = targetElement.classList;
 
-  // convert faqAnswers from HTMLcolletion to an array and loop
-  [...faqAnswers].forEach((answer) => {
-    answerClasses = answer.classList;
-    if (!answerClasses.contains("closed")) {
-      answerClasses.add("closed");
+  // if the element is not closed then close it
+  // else close every element and then open the current element
+  if (!classList.contains("closed")) {
+    classList.add("closed");
+  } else {
+    addClassToAll(targetElementClassname);
+    classList.remove("closed");
+  }
+}
+
+// function to add "closed" class to all element with elementClassName
+function addClassToAll(elementClassName) {
+  const targetElements = document.getElementsByClassName(elementClassName);
+
+  // convert targetElements from HTMLcolletion to an array and loop
+  [...targetElements].forEach((element) => {
+    classnames = element.classList;
+    if (!classnames.contains("closed")) {
+      classnames.add("closed");
     }
   });
 }
